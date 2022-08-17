2a855cba773ab4b51a35e67defcaa979b8d5f1ac
Bakeza <dbakeza2002@gmail.com>
2022-08-17 14:29:00 +0300

    handle countries values and put them in the drop down menu, put current date as a defult value Relates #12

--- public/js/dom.js
+++ public/js/dom.js
@@ -1,8 +1,23 @@
 const tbody = document.querySelector('#tbody');
+const select = document.querySelector('#input');
+const country = document.querySelector('#country');
+const day = document.querySelector('#day');
+const month = document.querySelector('#month');
+const year = document.querySelector('#year');
+const currentDate = new Date();
+const dayValue = currentDate.getDate();
+const monthValue = currentDate.getMonth() + 1;
+const yearValue = currentDate.getFullYear();
+day.value = dayValue;
+month.value = monthValue;
+year.value = yearValue;
 
-function createTableElement(element, parent, content) {
+function createTableElement(element, parent, content, value) {
   const childEle = document.createElement(`${element}`);
   childEle.textContent = content;
+  if (element === 'option') {
+    childEle.setAttribute('value', value);
+  }
   parent.appendChild(childEle);
   return childEle;
 }
@@ -12,11 +27,20 @@ function createTableElement(element, parent, content) {
 console.log('hello from dom');
 fetch('/countries')
   .then((res) => res.json())
-  .then((res) => console.log(res, 'countriesssss'))
+  .then(
+    (res) => {
+      for (let i = 0; i < res.countries.length; i++) {
+        createTableElement('option', select, res.countries[i].name, res.countries[i].code);
+      }
+    },
+  )
+
   .catch(console.error);
 
-  fetch('/currentLocation')
+fetch('/currentLocation')
   .then((res) => res.json())
-  .then((res) => console.log(res, 'locationnnnnn'))
+  .then((res) => {
+    console.log(res, 'locationnnnnn');
+    country.textContent = res;
+  })
   .catch(console.error);
- 
